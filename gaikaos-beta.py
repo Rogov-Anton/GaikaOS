4f8adcda4b090d3fa1ae0c786fce2c4097573023
Rogov-Anton <anton.rogov.1210@mail.ru>
2025-06-05 14:08:32 +0300

    Added features

--- gaikaos-beta.py
+++ gaikaos-beta.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3
+
+
 class FileSystem:
     def __init__(self):
-        self.files = [
-            Directory('bin'),
-            Directory('home')
-        ]  # корневой каталог
-        self.names = ['bin', 'home']  # строки с именами существующих файлов
+        self.files = []
+        self.names = []
 
     def find_directory(self, path: str):
         if path == '/':
@@ -26,17 +26,61 @@ class FileSystem:
     def make_directory(self, name: str, path: str):
         directory = self.find_directory(path)
         if not directory:
-            return f'mkdir: unable to create directory `{path}`: No such file or directory'
-        if name in directory.names:
-            return f'mkdir: unable to create directory `{name}`: File exists'
+            return f'mkdir: no such file or directory'
+        for elem in directory.files:
+            if (isinstance(elem, Directory) or isinstance(elem, File)) and elem.name == name:
+                return f'mkdir: file exists'
         directory.files.append(Directory(name))
         directory.names.append(name)
         return True
 
     def list_directory(self, path: str):
         directory = self.find_directory(path)
+        if not directory:
+            return f'lsdir: no such file or directory'
         return directory.files
 
+    def remove_directory(self, path: str, name: str):
+        directory = self.find_directory(path)
+        if not directory:
+            return f'rmdir: no such file or direcotry'
+        for elem in directory.files:
+            if isinstance(elem, Directory) and elem.name == name:
+                directory.names.remove(name)
+                directory.files.remove(elem)
+                return None
+        else:
+            return f'rmdir: no such file or direcotry'
+
+    def make_file(self, name: str, path: str):
+        directory = self.find_directory(path)
+        if not directory:
+            return f'touch: no such file or directory'
+        for elem in directory.files:
+            if isinstance(elem, Directory) and elem.name == name:
+                return f'touch: it`s a directory'
+            if isinstance(elem, File) and elem.name == name:
+                return None
+        else:
+            directory.files.append(File(name, ''))
+            directory.names.append(name)
+            return None
+
+    def write_file(self, text: str, path: str):
+        parts = path.split('/')
+        directory = self if len(parts) == 1 else self.find_directory('/'.join(parts[:len(parts) - 1]))
+        name_of_file = parts[-1]
+        for elem in directory.files:
+            if isinstance(elem, File) and elem.name == name_of_file:
+                elem.text = text
+                return None
+            elif isinstance(elem, Directory) and elem.name == name_of_file:
+                return f'write: it`s a direcotry'
+        else:
+            return f'write: no such file or directory'
+
+    def read_file(self, path: str):
+        pass
 
 class Directory:
     def __init__(self, name):
@@ -51,6 +95,30 @@ class File:
         self.text = text
 
 
+def print_fetch():
+    blue_color = "\033[34m"
+    reset_color = "\033[0m"
+
+    ascii_art = [
+        "         ____     ",
+        "        / __ \\    ",
+        "       / /  \\ \\   ",
+        "       \\ \\__/ /   ",
+        "        \\____/    "
+    ]
+
+    info_text = [
+        "GAIKA-FETCH",
+        "-----------",
+        "OS   GaikaOS-beta",
+        "VER  0.0.0.2",
+        "HST  GaikaPC"
+    ]
+
+    for art_line, info_line in zip(ascii_art, info_text):
+        print(f"{blue_color}{art_line}{reset_color}   {info_line}")
+
+
 def execute_command(filesystem: FileSystem, text: str):
     if not text:
         return
@@ -60,16 +128,43 @@ def execute_command(filesystem: FileSystem, text: str):
         name = parts[1]
         path = parts[2]
         result = filesystem.make_directory(name, path)
-        if result != True:
+        if not result:
             print(result)
     elif command == 'ls':
         path = parts[1]
         result = filesystem.list_directory(path)
+        if not isinstance(result, list):
+            print(result)
+            return
         for elem in result:
             if isinstance(elem, Directory):
                 print(f"\033[34m{elem.name}\033[0m")
             else:
                 print(elem.name)
+    elif command == 'rmdir':
+        path = parts[1]
+        name = parts[2]
+        result = filesystem.remove_directory(path, name)
+        if not result is None:
+            print(result)
+    elif command == 'touch':
+        path = parts[2]
+        name = parts[1]
+        result = filesystem.make_file(name, path)
+        if not result is None:
+            print(result)
+    elif command == 'write':
+        #   0     1     2     3
+        # write Hello World! file
+        text = parts[1:len(parts) - 2]
+        path = parts[-1]
+        result = filesystem.write_file(text, path)
+        if not result is None:
+            print(result)
+    elif command == 'fetch':
+        print_fetch()
+    else:
+        print('unknown command')
 
 
 filesystem = FileSystem()
@@ -80,5 +175,5 @@ if R:
         execute_command(filesystem, command)
         command = input('-> ')
 else:
-    command = 'mkdir home /'
+    command = ''
     execute_command(filesystem, command)
